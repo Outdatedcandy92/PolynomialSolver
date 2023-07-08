e20a9873c3c77bae3bf3dc6ffeaef49d187eb152
Outdatedcandy2 <114146070+Outdatedcandy2@users.noreply.github.com>
2023-07-09 01:14:42 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from sympy import symbols, solve
+
+page_title = "Polynomial Solver"
+
+layout = "centered"
+
+# -----
+st.set_page_config(page_title=page_title,layout=layout)
+st.title(page_title)
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+with st.form("entry_form",clear_on_submit=True):
+    expr = st.text_area("",placeholder="Enter A Polynomial")     
+    submitted = st.form_submit_button("Answer")
+
+    if submitted:
+        x = symbols('x')
+        if expr.startswith("x"):
+            new= "1"+expr
+            print(new)
+            final_expr = new.replace('x', '*x' )
+            sol = solve(final_expr)
+            st.write(sol)
+        
+                 
+
+
+        else:
+            final_expr = expr.replace('x', '*x' )
+            sol = solve(final_expr)
+
+            st.write(sol)
+            
+            
+        print(sol)
